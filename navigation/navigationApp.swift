fdd84f68bd8f12fc1eaee32af2278fae760838c1
Ambar <ambar.2703.sanchez@gmail.com>
2025-07-24 10:59:15 -0600

    Initial Commit

---
+++ navigation/navigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  navigationApp.swift
+//  navigation
+//
+//  Created by Ámbar Aguilar Sánchez on 24/07/25.
+//
+
+import SwiftUI
+
+@main
+struct navigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
